08aefb19d3dd1ce152d1e33eb994d61f79bbe197
elentr <tr.13.elena@gmail.com>
2025-07-22 23:29:41 +0300

    Inital commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+// import toast, { Toaster } from "react-hot-toast";
+import css from "./App.module.css";
+// import ErrorMessage from "../ErrorMessage/ErrorMessage";
+// import Loader from "../Loader/Loader";
+// import MovieGrid from "../MovieGrid/MovieGrid";
+// import MovieModal from "../MovieModal/MovieModal";
+// import SearchBar from "../SearchBar/SearchBar";
+// import { fetchMovies } from "../../services/movieService";
+// import type { Movie } from "../../types/movie";
+// import { keepPreviousData, useQuery } from "@tanstack/react-query";
+// import ReactPaginate from "react-paginate";
+
+export default function App() {}
